7417f8cf019dd50029dea752cbaee1c0a4a83456
Amit Sharma <amitsharma1809@yahoo.com>
2020-03-05 00:53:16 -0800

    Update UDP

--- udp/udp_client.py
+++ udp/udp_client.py
@@ -4,32 +4,39 @@ import random
 UDP_IP = '127.0.0.1'
 UDP_PORT = 4000
 BUFFER_SIZE = 1024
-#MESSAGE = "1,2,3,4,5"
 FILE_TO_UPLOAD = "upload.txt"
 
 
 def send(id=0):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        upload_file(s)
+        with open(FILE_TO_UPLOAD) as file:
+            for line in file:
+                #Resend every packet 5 times if SEQUENCE numbers not matches
+                for i in range(6):
+                    SEQ_NO = random.sample(range(1, 10000), 1)
+                    print(f'Sequence number sent:{SEQ_NO}')
+                    message = str(SEQ_NO)+":"+line
+                    s.sendto(f"{id}:{message}".encode(), (UDP_IP, UDP_PORT))
+                    data, ip = s.recvfrom(BUFFER_SIZE)
+                    data = data.decode(encoding="utf-8").strip()
+                    SEQ_RECVD = data[:]
+                    #SEQ_RECVD=[000]
+                    print(f'Sequence number received from server:{SEQ_RECVD}')
+                    #Resend message if send and recived SEQUENCE no not matches
+                    if str(SEQ_NO) == str(SEQ_RECVD):
+                        print("SEQUENCE NO matched")
+                        break;
+
+                else:
+                    print("Acknowledgment not received from server after multiple trials, not sending other packets")
+                    break;
+
+
 
     except socket.error:
         print("Error! {}".format(socket.error))
         exit()
-
-def upload_file(s):
-    with open(FILE_TO_UPLOAD) as file:
-        for line in file:
-            line = line.strip()
-            # print(line)
-            # sequence, message = line.split(":")
-            # print(sequence)
-            # print(message)
-            SEQ_NO = random.sample(range(1, 10000), 1)
-            print(SEQ_NO)
-            s.sendto(f"{id}:{line}".encode(), (UDP_IP, UDP_PORT))
-            data, ip = s.recvfrom(BUFFER_SIZE)
-            print("received data: {}: {}".format(ip, data.decode()))
 
 
 
@@ -38,13 +45,3 @@ def get_client_id():
     return id
 
 send(get_client_id())
-
-'''
-        s.sendto(f"{id}:{MESSAGE}".encode(), (UDP_IP, UDP_PORT))
-        data, ip = s.recvfrom(BUFFER_SIZE)
-        print("received data: {}: {}".format(ip, data.decode()))
-    except socket.error:
-        print("Error! {}".format(socket.error))
-        exit()
-
-'''
--- udp/udp_server.py
+++ udp/udp_server.py
@@ -9,14 +9,17 @@ MESSAGE = "pong"
 def listen_forever():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(("", UDP_PORT))
+    print(f'Server started at port {UDP_PORT}')
 
     while True:
         # get the data sent to us
         data, ip = s.recvfrom(BUFFER_SIZE)
-        #print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
-        print(data.decode(encoding="utf-8").strip())
-        # reply back to the client
-        s.sendto(MESSAGE.encode(), ip)
+        print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
+        data = data.decode(encoding="utf-8").strip()
+        client, SEQ_NO, ID, message = data.split(':',-1)
+        print(f'Sequence number received: {SEQ_NO}')
+        # reply back received sequence to the client
+        s.sendto(SEQ_NO.encode(), ip)
 
 
 listen_forever()
